5861be5da62936005c37ab7cccbc150e81757f38
Paul Lysak <paul.lysak@gmail.com>
2013-07-04 23:16:37 +0300

    Integration tests DB create/drop

--- project/DB.scala
+++ project/DB.scala
@@ -1,11 +1,32 @@
+import com.typesafe.config.ConfigFactory
+import java.io.File
+import scala.slick.jdbc.StaticQuery
 import scala.slick.session.Database
 import scala.slick.driver.PostgresDriver.simple._
 import Database.threadLocalSession
 
 
 object DB {
+  val db = Database.forURL(config.getString("database.setupUrl"), driver = config.getString("database.driver"))
+
   def create {
-    println("TODO: create DB")
-//    Database.forU
+     drop
+     db.withSession {
+       println(s"Creating DB $dbName...")
+       StaticQuery.updateNA(s"CREATE DATABASE $dbName").execute()
+     }
   }
+
+  def drop {
+    db.withSession {
+      println(s"Dropping DB $dbName...")
+      StaticQuery.updateNA(s"DROP DATABASE IF EXISTS $dbName").execute()
+    }
+
+  }
+
+  lazy val dbName = config.getString("database.name")
+
+  lazy val config = ConfigFactory.parseFile(new File("it.conf"))
+
 }
--- project/build.sbt
+++ project/build.sbt
@@ -7,6 +7,7 @@ scalaVersion := "2.10.1"
 libraryDependencies ++= List(
   // use the right Slick version here:
   "com.typesafe.slick" %% "slick" % "1.0.1",
+  "com.typesafe" % "config" % "1.0.2",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
   "com.h2database" % "h2" % "1.3.166",
   "postgresql" % "postgresql" % "9.1-901.jdbc4"
