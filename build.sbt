8622161fd5af1a6ffb9b1205c10606ed4ee2f134
Paul Lysak <paul.lysak@gmail.com>
2013-07-05 23:46:54 +0300

    Expenses file parsing

--- build.sbt
+++ build.sbt
@@ -8,8 +8,9 @@ libraryDependencies ++= List(
   // use the right Slick version here:
   "com.typesafe.slick" %% "slick" % "1.0.1",
   "com.typesafe" % "config" % "1.0.2",
+  "com.github.tototoshi" %% "scala-csv" % "0.8.0",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
-  "com.h2database" % "h2" % "1.3.166",
+//  "com.h2database" % "h2" % "1.3.166",
   "postgresql" % "postgresql" % "9.1-901.jdbc4",
   "org.specs2" %% "specs2" % "1.14" % "test,it"
 )
--- src/main/scala/finloader/ExpensesLoader.scala
+++ src/main/scala/finloader/ExpensesLoader.scala
@@ -1,8 +1,14 @@
 package finloader
 
 import scala.slick.session.Database
-import java.io.{InputStream, Reader}
+import java.io.{File, InputStream, Reader}
 import java.net.URL
+import com.github.tototoshi.csv.CSVReader
+import finloader.domain.{Expenses, Expense}
+import java.sql.Date
+import scala.slick.driver.PostgresDriver.simple._
+import Database.threadLocalSession
+
 
 /**
  * @author Paul Lysak
@@ -12,7 +18,32 @@ import java.net.URL
 class ExpensesLoader(db: Database) {
   def load(source: URL) {
     println(s"TODO load from $source")
+    val idPrefix = "0_"
+    val reader = CSVReader.open(new File(source.toURI))
+    reader.toStream() match {
+      case firstRow #:: body =>
+        val p = firstRow.zipWithIndex.toMap
+        for(row <- body) {
+          val r = row.toIndexedSeq
+          val expense = Expense(id = idPrefix+r(p("id")),
+            date = new Date(2013, 06, 10), //TODO parse date
+            amount = (r(p("amount")).toDouble * 100).toLong,
+            category = r(p("category")),
+            comment = r(p("comment")))
+          println("row: "+expense)
+          save(expense)
+        }
+      case _ =>
+        println("can't find first line")
+    }
+//    println("content = "+reader.all())
     //TODO
+  }
+
+  private def save(expense: Expense) {
+    db.withSession {
+      Expenses.insert(expense)
+    }
   }
 
 }
